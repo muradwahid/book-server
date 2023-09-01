2a7c75c171561ec7b0095fd144e62dee62d92f11
muradwahid <muradwahid334@gmail.com>
2023-09-01 15:46:14 +0600

    server project setup

---
+++ src/app/modules/book/book.model.ts
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+import { IBook } from "./book.interface";
+
+const BookSchema = new Schema<IBook>({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: { type: String, required: true },
+  publicationDate: { type: String, required: true },
+  Reviews:{type:String}
+})
+
+export const Book=mongoose.model('Book',BookSchema);
